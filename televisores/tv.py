fd3e4ab612ddac7028f539b75face0bdce935dde
jonatan8254 <josanchezv@unal.edu.co>
2023-02-27 23:34:29 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -1,6 +1,6 @@
 class TV:
     numTV = 0
-    
+
     def __init__(self, marca, estado):
         self.marca = marca
         self.canal = 1
@@ -9,67 +9,68 @@ class TV:
         self.volumen = 1
         self.control = None
         TV.numTV += 1
-    
+
     def setMarca(self, marca):
         self.marca = marca
-    
+
     def getMarca(self):
         return self.marca
-    
+
     def setControl(self, control):
         self.control = control
-    
+
     def getControl(self):
         return self.control
-    
+
     def setPrecio(self, precio):
         self.precio = precio
-    
+
     def getPrecio(self):
         return self.precio
-    
+
     def setVolumen(self, volumen):
-        if self.estado and volumen >= 0 and volumen <= 7:
+        if self.estado and 0 <= volumen <= 7:
             self.volumen = volumen
-    
+
     def getVolumen(self):
         return self.volumen
-    
+
     def setCanal(self, canal):
-        if self.estado and canal >= 1 and canal <= 120:
+        if self.estado and 1 <= canal <= 120:
             self.canal = canal
-    
+
     def getCanal(self):
         return self.canal
-    
+
     def turnOn(self):
         self.estado = True
-    
+
     def turnOff(self):
         self.estado = False
-    
+
     def getEstado(self):
         return self.estado
-    
-    @staticmethod
-    def getNumTV():
-        return TV.numTV
-    
-    def setNumTV(self, numTV):
-        TV.numTV=numTV
-    
+
     def canalUp(self):
         if self.estado and self.canal < 120:
             self.canal += 1
-    
+
     def canalDown(self):
         if self.estado and self.canal > 1:
             self.canal -= 1
-    
+
     def volumenUp(self):
         if self.estado and self.volumen < 7:
             self.volumen += 1
-    
+
     def volumenDown(self):
         if self.estado and self.volumen > 0:
             self.volumen -= 1
+
+    @classmethod
+    def setNumTV(cls, num):
+        cls.numTV = num
+
+    @classmethod
+    def getNumTV(cls):
+        return cls.numTV
